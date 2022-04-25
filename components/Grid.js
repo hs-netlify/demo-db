f8d29dd09facb346cf2d97d87371ed79902919f0
Henry Smith <henry.smith@netlify.com>
2022-04-25 17:37:13 +0100

    Grid styles

--- components/Grid.js
+++ components/Grid.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Grid = ({ children }) => (
   <div className="flex w-full items-center justify-center">
-    <div className="w-full grid grid-cols-auto-fit justify-center p-4 transition-all duration-75 ">
+    <div className="w-full grid grid-cols-auto-fit justify-center p-4  ">
       {children}
     </div>
   </div>
