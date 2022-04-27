85403530a3e03afe5fc5013a63fd884717a69488
Henry Smith <henry.smith@netlify.com>
2022-04-27 19:20:31 +0100

    Demo detail styling

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => (
-  <div className="flex h-20  text-white items-center px-20 bg-black shadow">
-    <ul>
+  <div className="flex h-20 items-center px-32 justify-between">
+    <div className="flex w-full justify-start">
       <Link href="/" passHref>
-        <li className="cursor-pointer">Home</li>
+        <div className="rounded-full hover:opacity-80 cursor-pointer text-xl border-2 flex justify-center items-center h-12 w-12  border-teal-600 text-teal-600">
+          <FontAwesomeIcon className="w-6" icon={faHome} />
+        </div>
       </Link>
-    </ul>
+    </div>
+    <div className="text-4xl w-full text-center">Netlify SE Demo Hub</div>
+    <div className="w-full flex justify-end">
+      <img
+        className="h-36"
+        src={`${process.env.NEXT_PUBLIC_BASE_URL}/Netlify-Logo.svg`}
+      />
+    </div>
   </div>
 );
 
